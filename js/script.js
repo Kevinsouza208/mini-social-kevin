51298ba3c305f9989d4e5da3b9da9429b03fc3b0
Kevinsouza208 <kevin.9305219@aluno.mg.gov.br>
2026-04-07 18:31:13 +0000

    completado

--- js/script.js
+++ js/script.js
@@ -1,39 +1,48 @@
 let likeCount = 0;
-let curtido = false; //flag booleana
+let curtido = false;
+
+let dislikeCount = 0;
+let descurtido = false;
 
 function curtir() {
-  
-if(curtido == false){
-  likeCount++;
-  curtido = true;
-  document.getElementById("likeCount").innerText = likeCount;
-}else{
-  likeCount--;
-  curtido = false;
-  document.getElementById("likeCount").innerText = likeCount;
-
+  if (curtido == false) {
+    
+    if (descurtido == true) {
+      descurtido = false;
+      dislikeCount--;
+    }
+    likeCount++;
+    curtido = true;
+  } else {
+    
+    likeCount--;
+    curtido = false;
+  }
+  atualizarInterface();
 }
 
+function descurtir() {
+  if (descurtido == false) {
+    
+    if (curtido == true) {
+      curtido = false;
+      likeCount--;
+    }
+    dislikeCount++;
+    descurtido = true;
+  } else {
+    
+    dislikeCount--;
+    descurtido = false;
+  }
+  atualizarInterface();
+}
 
+function atualizarInterface() {
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
-
-function descurtir() {
-  
-if(descurtido == false){
-  likeCount++;
-  curtido = true;
-  document.getElementById("likeCount").innerText = deslikeCount;
-}else{
-  deslikeCount--;
-  curtido = false;
-  document.getElementById("likeCount").innerText = likeCount;
-
-}
-
-
-}
-
-document.getElementById("likeBtn").addEventListener("click", descurtir);
+document.getElementById("dislikeBtn").addEventListener("click", descurtir);
 
